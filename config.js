584190412b2959828e2d645944db2cfb85255b85
darkshadow team <darkshadowteam39@gmail.com>
2024-12-24 11:05:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "3RZ1Ta4a#TrKZJEhv5wuEtnzNpMDyZ09ZciTW_qDYx4l6Gi9rCmU", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
